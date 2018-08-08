7bba313e557a2050bba2a87b2d3a2abad60d1a6d
ervinismu <social.ervin@gmail.com>
2018-08-08 16:37:48 +0700

    [erpin] add crud

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,21 +1,42 @@
 class ProductController < ApplicationController
-    
+    skip_before_action :verify_authenticity_token  
+    before_action :find_product, only: [:show, :destroy]
+
     def index
         products = Product.all
         render json: products.blank? ? { data: "product not found"} : products, status: :ok    
     end
 
     def show
-        product = Product.find_by(id: params[:id])
-        render json: product.blank? ? { data: "product not found"} : product, status: :ok
+        render json: @product.blank? ? { data: "product not found"} : @product, status: :ok
     end
 
-    def delete
-    
+    def destroy
+        if @product
+            @product.destroy!
+            render json: {message: "success deleted product"} , status: :unprocessable_entity            
+        else
+            render json: {message: "failed deleted product"} , status: :unprocessable_entity
+        end
     end
 
-    def edit
-    
+    def create
+        product = Product.new(product_params)
+        if product.save
+          render json: { message: "success inserted" }, status: :created
+        else
+          render json: { message: "failed inserted" }, status: :unprocessable_entity
+        end
     end
 
+    private
+
+    def find_product
+        @product = Product.find_by(id: params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :description)
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # get '/products', to: "product#index"
-  resources :product
+  get "/products", to: "product#index"
+  get "/product/detail", to: "product#show"
+  post "/product", to: "product#create"
+  delete "/product/delete", to: "product#destroy"
 
 end
