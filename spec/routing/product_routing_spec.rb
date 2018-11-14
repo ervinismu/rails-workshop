55ed8b2e146572082bb7fca0f4009adebd4ce628
ervinismu <social.ervin@gmail.com>
2018-11-15 00:00:30 +0700

    edit unit testing

--- spec/routing/product_routing_spec.rb
+++ spec/routing/product_routing_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ProductController, type: :routing do
-	describe 'get all product routing' do
-		it "routes to /products to product#index" do
-			expect(get: '/products').to route_to('product#index')
-		end
-	end
+	# describe 'get all product routing' do
+	# 	it "routes to /products to product#index" do
+	# 		expect(get: '/products').to route_to('product#index')
+	# 	end
+	# end
 
-	describe 'get detail product routing' do
-		it "routes to /product/detail to product#show" do
-			expect(get: '/product/detail').to route_to('product#show')
-		end
-	end
+	# describe 'get detail product routing' do
+	# 	it "routes to /product/detail to product#show" do
+	# 		expect(get: '/product/detail').to route_to('product#show')
+	# 	end
+	# end
 
-	describe 'post new product routing' do
-		it "routes to /product to product#create" do
-			expect(post: '/product').to route_to('product#create')
-		end
-	end
+	# describe 'post new product routing' do
+	# 	it "routes to /product to product#create" do
+	# 		expect(post: '/product').to route_to('product#create')
+	# 	end
+	# end
 
-	describe 'delete products rpouting' do
-		it "routes to /product/delete to product#delete" do
-			expect(delete: '/product/delete').to route_to('product#destroy')
-		end
-	end
+	# describe 'delete products rpouting' do
+	# 	it "routes to /product/delete to product#delete" do
+	# 		expect(delete: '/product/delete').to route_to('product#destroy')
+	# 	end
+	# end
 end
